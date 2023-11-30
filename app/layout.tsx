ed8d93be87eccc11bbacafe72dc56d6fae65475a
snakechickensoup <141708304+snakechickensoup@users.noreply.github.com>
2023-11-30 13:38:02 +0900

    add tailwind config

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,19 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
-import { Providers } from "./provider";
+import type { Metadata } from 'next';
+import { Noto_Sans_KR } from 'next/font/google';
+import './globals.css';
+import { Providers } from './provider';
 
-const inter = Inter({ subsets: ["latin"] });
+const notoSans = Noto_Sans_KR({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "DevBook",
-  description: "DevBook",
+  title: 'DevBook',
+  description: 'DevBook',
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ko-KR">
-      <body className={inter.className}>
+      <body className={notoSans.className}>
         <Providers>{children}</Providers>
       </body>
     </html>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
@@ -7,14 +7,25 @@ const config: Config = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    colors: {
+      white: '#fff',
+      lightGray: '#D6D6D6',
+      gray: '#878484',
+      black: '#1C1C1C',
+      green: '#A7D6B7',
+      deepGreen: '#31854D',
+      red: '#E34646',
     },
+    fontSize: {
+      xs: '0.75rem' /* 12px */,
+      sm: '0.875rem' /* 14px */,
+      base: '1rem' /* 16px */,
+      lg: '1.125rem' /* 20px */,
+      xl: '1.5rem' /* 24px */,
+      '2xl': '2.25rem' /*3 6px */,
+    },
+    extend: {},
   },
   plugins: [],
-}
-export default config
+};
+export default config;
