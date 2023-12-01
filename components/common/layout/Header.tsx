689fa0eb3b57d23c70d2e1f0090499b3b3646e36
snakechickensoup <141708304+snakechickensoup@users.noreply.github.com>
2023-12-01 11:01:21 +0900

    modify header component

--- components/common/layout/Header.tsx
+++ components/common/layout/Header.tsx
@@ -1,11 +1,10 @@
 'use client';
 
 import React from 'react';
-import SunIcon from '@/assets/icons/sun.svg';
-import UserIcon from '@/assets/icons/user.svg';
 import Button from '../button/Button';
 import { signIn, useSession } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
+import Icon from '../Icon';
 interface Props {
   page: string;
 }
@@ -14,25 +13,45 @@ const Header = ({ page }: Props) => {
   const router = useRouter();
   const { data } = useSession();
 
-  const contents = page === 'main' && (
-    <>
-      <h1 className="text-2xl font-bold">DevBook</h1>
-      <section className="flex items-center gap-4">
-        <Button btnStyle="btn-ghost" handleClick={() => {}}>
-          <SunIcon className="h-6 w-6 fill-deepGreen" />
-        </Button>
-        {data !== null ? (
-          <Button btnStyle="btn-ghost" handleClick={() => router.push('/user')}>
-            <UserIcon className="h-7 w-7 fill-deepGreen" />
+  let contents = <></>;
+  switch (page) {
+    case 'main':
+      contents = (
+        <>
+          <h1 className="text-2xl font-bold">DevBook</h1>
+          <section className="flex items-center gap-4">
+            <Button btnStyle="btn-ghost" handleClick={() => {}}>
+              <Icon name="sun" className="h-8 w-8 fill-deepGreen" />
+            </Button>
+            {data !== null ? (
+              <Button btnStyle="btn-ghost" handleClick={() => router.push('/user')}>
+                <Icon name="user" className="h-8 w-8 fill-deepGreen" />
+              </Button>
+            ) : (
+              <Button handleClick={signIn} btnStyle="btn-primary">
+                로그인
+              </Button>
+            )}
+          </section>
+        </>
+      );
+      break;
+    case 'user':
+      contents = (
+        <>
+          <Button btnStyle="btn-ghost" handleClick={() => router.push('/')}>
+            <Icon name="arrowLeft" className="h-8 w-8  stroke-deepGreen" />
           </Button>
-        ) : (
-          <Button handleClick={signIn} btnStyle="btn-primary">
-            로그인
+          <Button btnStyle="btn-ghost" handleClick={() => {}}>
+            <Icon name="bell" className="h-8 w-8 stroke-deepGreen" />
           </Button>
-        )}
-      </section>
-    </>
-  );
+        </>
+      );
+      break;
+    default:
+      break;
+  }
+
   return (
     <header className="flex items-center justify-between px-6 py-4 text-deepGreen">
       {contents}
