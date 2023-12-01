fd930b65f94c284007fb7e7c573ac918e4632ada
snakechickensoup <141708304+snakechickensoup@users.noreply.github.com>
2023-12-01 19:27:45 +0900

    modify user page

--- components/user/QuestionsList.tsx
+++ components/user/QuestionsList.tsx
@@ -1,18 +1,15 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import QuestionsListTab from './QuestionsListTab';
-import { getFilteredQuestions } from '@/firebase/questions';
 import IQuestion from '@/types/questions';
 import Question from './Question';
 
-const QuestionsList = ({ user }: IUser) => {
-  console.log(user.id, 'user:');
+interface Props {
+  questions: IQuestion[];
+}
+
+const QuestionsList = (props: Props) => {
+  const { questions } = props;
   const [tab, setTab] = useState<number>(0);
-
-  const [questions, setQuestions] = useState<IQuestion[]>([]);
-
-  useEffect(() => {
-    // getFilteredQuestions({ userId: user.id }).then((res) => console.log('필터링된 데이터 ', res));
-  }, []);
 
   const handleTab = (n: number) => {
     setTab(n);
