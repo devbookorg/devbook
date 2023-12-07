255b0b6f896b8adc3b44d76b548df69f4a3335d5
snakechickensoup <141708304+snakechickensoup@users.noreply.github.com>
2023-12-07 14:03:05 +0900

    feat pagination

---
+++ hooks/usePagination.ts
@@ -0,0 +1,33 @@
+import { useEffect, useState } from 'react';
+
+export const usePagination = (total: number) => {
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const [startPage, setStartPage] = useState<number>(1);
+  const [endPage, setEndPage] = useState<number>(1);
+  const pageRange = 5;
+  const totalPages = Math.ceil(total / 3);
+
+  const handleChangePage = (page: number) => {
+    setCurrentPage(page);
+
+    const half = Math.floor(pageRange / 2);
+    if (page <= half) {
+      setStartPage(1);
+      setEndPage(totalPages > pageRange ? pageRange : totalPages);
+    } else if (totalPages - page < half) {
+      setStartPage(totalPages - pageRange + 1);
+      setEndPage(totalPages);
+    } else {
+      setStartPage(page - half);
+      setEndPage(page + half);
+    }
+  };
+
+  useEffect(() => {
+    if (totalPages > Math.floor(pageRange / 2)) {
+      setEndPage(totalPages > pageRange ? pageRange : totalPages);
+    }
+  }, [total, totalPages]);
+
+  return { currentPage, startPage, endPage, totalPages, handleChangePage };
+};
