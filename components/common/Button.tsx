ec75b763e89bb898160c6a290adcf144ad9b673d
snakechickensoup <141708304+snakechickensoup@users.noreply.github.com>
2023-12-08 16:41:00 +0900

    modify buttonClass func

--- components/common/Button.tsx
+++ components/common/Button.tsx
@@ -1,4 +1,4 @@
-import { ButtonStyle, getbuttonClass } from '@/types/buttons';
+import { ButtonStyle } from '@/types/buttons';
 
 interface Props {
   children: React.ReactNode;
@@ -8,6 +8,22 @@ interface Props {
   type?: 'button' | 'submit' | 'reset';
   disabled?: boolean;
 }
+
+export const getbuttonClass = (style: ButtonStyle) => {
+  const [buttonSize, type, color] = style.split('-');
+  const size = buttonSize === 'sm' ? 'btn-sm' : 'btn-lg';
+  switch (type) {
+    case 'fill':
+      return `${size} ${color === 'red' ? 'bg-red' : 'bg-deepGreen'} text-white`;
+    case 'line':
+      return `${size} border 
+      ${color === 'red' ? 'border-red' : 'border-deepGreen'} 
+      ${color === 'red' ? 'text-red' : 'text-deepGreen'} 
+      ${color === 'red' ? 'hover:bg-red' : 'hover:bg-deepGreen'} hover:text-white`;
+    case 'ghost':
+      return `${size}`;
+  }
+};
 
 const Button = ({ children, handleClick, styles, btnStyle, type, disabled }: Props) => {
   const buttonStyles = `${styles || ''} ${getbuttonClass(btnStyle)}`;
--- types/buttons.ts
+++ types/buttons.ts
@@ -5,16 +5,3 @@ type ButtonColor = 'gray' | 'deepGreen' | 'red';
 type CheckType<T> = T extends 'ghost' ? `${Exclude<ButtonType, 'ghost'>}-${ButtonColor}` : 'ghost';
 
 export type ButtonStyle = `${ButtonSize}-${CheckType<ButtonType>}`;
-
-export const getbuttonClass = (style: ButtonStyle) => {
-  const [buttonSize, type, color] = style.split('-');
-  const size = buttonSize === 'sm' ? 'btn-sm' : 'btn-lg';
-  switch (type) {
-    case 'fill':
-      return `${size} bg-${color} text-white hover:bg-${color}`;
-    case 'line':
-      return `${size} border border-${color} text-${color} hover:bg-${color} hover:text-white`;
-    case 'ghost':
-      return `${size} hover:scale-125`;
-  }
-};
