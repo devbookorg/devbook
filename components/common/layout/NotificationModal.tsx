3aedebe983abcb062cf8d07c037a3e6a30b60057
snakechickensoup <141708304+snakechickensoup@users.noreply.github.com>
2023-12-11 13:27:15 +0900

    modify notification

--- components/common/layout/NotificationModal.tsx
+++ components/common/layout/NotificationModal.tsx
@@ -2,19 +2,27 @@ import { userState } from '@/recoil/user';
 import { NotificationMessage } from '@/types/users';
 import formatUnixTime from '@/utils/functions/formatUnixTime';
 import Link from 'next/link';
-import React from 'react';
-import { useRecoilValue } from 'recoil';
+import React, { useEffect } from 'react';
+import { useRecoilValue, useSetRecoilState } from 'recoil';
 
-const NotificationItem = (props: NotificationMessage) => {
-  const { approved, approvedDate, questionTitle, rejectionMessage } = props;
+interface Props {
+  closeModal: () => void;
+}
+
+const NotificationItem = (props: NotificationMessage & Props) => {
+  const { approved, approvedDate, questionTitle, rejectionMessage, closeModal } = props;
   const isApproved = approved === 1 ? '승인' : '거부';
   return (
     <>
       <li className="py-2">
-        <Link href="/user">
+        <Link href="/user" onClick={closeModal}>
           <div className="flex flex-col gap-0.5">
-            <h3 className={`text-xs ${isApproved === '승인' ? 'text-deepGreen' : 'text-red'}`}>
-              - {isApproved} -
+            <h3
+              className={`text-xs font-bold ${
+                isApproved === '승인' ? 'text-deepGreen' : 'text-red'
+              }`}
+            >
+              {isApproved}
             </h3>
             <p className="px-1 text-sm text-black">
               <span className="text-deepGreen">&#39;{questionTitle}&#39; </span>
@@ -35,13 +43,18 @@ const NotificationItem = (props: NotificationMessage) => {
   );
 };
 
-const Notification = () => {
+const Notification = (props: Props) => {
+  const { closeModal } = props;
   const { notificationMessages } = useRecoilValue(userState);
+  const setUserState = useSetRecoilState(userState);
+  useEffect(() => {
+    setUserState((prev) => ({ ...prev, notification: false }));
+  }, []);
   return (
     <section className="absolute right-0 top-0 z-[40] flex h-full w-full max-w-[36em] flex-col items-start gap-2 bg-white p-4 pt-16">
       <ul className="top-0 h-full w-full overflow-scroll">
         {[...notificationMessages].reverse().map((e, i) => (
-          <NotificationItem key={e.approvedDate.toString() + i} {...e} />
+          <NotificationItem key={e.approvedDate.toString() + i} {...e} closeModal={closeModal} />
         ))}
       </ul>
     </section>
