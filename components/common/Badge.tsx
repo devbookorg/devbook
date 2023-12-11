8df2a9891c4127cdd49ec784e5dfc0f7acea967f
snakechickensoup <141708304+snakechickensoup@users.noreply.github.com>
2023-12-11 17:50:11 +0900

    modify badge component

--- components/common/Badge.tsx
+++ components/common/Badge.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 
 interface Props {
   value: string;
+  size?: 'sm' | 'lg';
 }
 
 const Badge = (props: Props) => {
-  const { value } = props;
+  const { value, size } = props;
   const bg = {
-    JS: 'bg-yellow text-black',
+    JS: 'bg-yellow',
     TS: 'bg-sky',
     HTML: 'bg-orange',
     CSS: 'bg-blue',
@@ -15,9 +16,22 @@ const Badge = (props: Props) => {
     NEXT: 'bg-black',
     CS: 'bg-deepGreen',
   };
+
+  const textColor = value === 'JS' ? 'text-black' : 'text-white';
+
+  if (size === 'lg') {
+    return (
+      <div
+        className={`w-fit rounded-xl px-2 py-1 text-xs leading-tight ${bg[value]} ${textColor} border border-black bg-opacity-90 hover:bg-opacity-100`}
+      >
+        {value}
+      </div>
+    );
+  }
+
   return (
     <div
-      className={`w-fit rounded-md px-1.5 py-0.5 text-xs leading-tight text-white ${bg[value]}  bg-opacity-90 hover:bg-opacity-100`}
+      className={`w-fit rounded-md px-1.5 py-0.5 text-xs leading-tight ${bg[value]} ${textColor} bg-opacity-90 hover:bg-opacity-100`}
     >
       {value}
     </div>
