3691a9e00bcec45f8c83934a3bf97ee1e2836796
snakechickensoup <141708304+snakechickensoup@users.noreply.github.com>
2023-12-12 18:14:50 +0900

    add pwa

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from 'next';
+import type { Metadata, Viewport } from 'next';
 import { Noto_Sans_KR } from 'next/font/google';
 import './globals.css';
 import { AppProviders } from './AppProvider';
@@ -20,6 +20,10 @@ export const metadata: Metadata = {
       },
     ],
   },
+};
+
+export const viewport: Viewport = {
+  themeColor: '#31854C',
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
