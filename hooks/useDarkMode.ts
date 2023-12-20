894cc97c8a8ace5a7a16877664d343f715041b4e
snakechickensoup <141708304+snakechickensoup@users.noreply.github.com>
2023-12-20 15:35:25 +0900

    delete theme recoil

--- hooks/useDarkMode.ts
+++ hooks/useDarkMode.ts
@@ -1,19 +1,14 @@
-import { themeState } from '@/recoil/theme';
-import { useEffect } from 'react';
-
-import { useRecoilState } from 'recoil';
+import { useState, useEffect } from 'react';
 
 export const useDarkMode = () => {
-  const [isDarkMode, setIsDarkMode] = useRecoilState(themeState);
+  const [isDarkMode, setIsDarkMode] = useState(null);
 
   useEffect(() => {
-    const theme = localStorage.theme;
-    setIsDarkMode(theme === 'dark');
+    setIsDarkMode(localStorage.theme === 'dark');
   }, []);
 
   const handleDarkMode = () => {
-    const currentTheme = localStorage.theme;
-    if (currentTheme !== 'dark') {
+    if (!isDarkMode) {
       setIsDarkMode(true);
       localStorage.theme = 'dark';
       document.documentElement.dataset.theme = 'dark';
--- recoil/theme.ts
+++
@@ -1,6 +0,0 @@
-import { atom } from 'recoil';
-
-export const themeState = atom<boolean>({
-  key: 'themeState',
-  default: null,
-});
